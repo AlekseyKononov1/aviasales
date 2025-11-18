06546b8b9926df3d2d14f3c8f0bcfdbb81be011e
Aleksey Kononov <alconmail@mail.ru>
2025-11-19 01:00:57 +0300

    setting up docker files and test connectivity between nodes {go app and db postgresql}

--- cmd/aviasales/main.go
+++ cmd/aviasales/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
 func main() {
-	//test docker setup
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	psqlInfo := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, user, password, dbname,
+	)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
+	}
+	defer db.Close()
+
+	for {
+		err = db.Ping()
+		if err == nil {
+			break
+		}
+		log.Println("Database not ready, retrying...")
+		time.Sleep(4 * time.Second)
+	}
+
+	rows, err := db.Query("SELECT count(*) FROM bookings.bookings")
+	if err != nil {
+		log.Fatalf("Query failed: %v", err)
+	}
+	defer rows.Close()
+
+	fmt.Println("Users in database:")
+	for rows.Next() {
+		var count int
+		if err := rows.Scan(&count); err != nil {
+			log.Fatalf("Row scan failed: %v", err)
+		}
+		fmt.Printf("rows in Bookings%v\n", count)
+	}
 }
